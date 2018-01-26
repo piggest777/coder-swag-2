0c5d892e91e8e921d9a2c9e5a439d6948464277a
Denis Rakitin <svinson@Deniss-MacBook.local>
2018-01-27 02:21:46 +0300

    Product Cell added

---
+++ coder-swag-2/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag-2
+//
+//  Created by Denis Rakitin on 27/01/2018.
+//  Copyright © 2018 piggest. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
